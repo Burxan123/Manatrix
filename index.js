3e478684f3473a40c95257d7bcffc243a1f21022
Burxan777 <117080755+Burxan777@users.noreply.github.com>
2024-04-26 02:08:52 +0400

    api e

--- index.js
+++ index.js
@@ -17,12 +17,6 @@ dotenv.config();
 
 const myapp = express();
 
-// CORS başlığını ekleyen middleware
-myapp.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://manatrixacademy.com");
-  next();
-});
-
 myapp.use(express.json());
 myapp.use(helmet());
 myapp.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
